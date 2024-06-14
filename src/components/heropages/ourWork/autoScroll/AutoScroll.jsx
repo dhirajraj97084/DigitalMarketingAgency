04ba37acacf06fc2d3352bab198e7d660cb7016e
patwalvijay <122560239+patwalvijay@users.noreply.github.com>
2024-06-14 19:33:48 +0530

    logo

--- src/components/heropages/ourWork/autoScroll/AutoScroll.jsx
+++ src/components/heropages/ourWork/autoScroll/AutoScroll.jsx
@@ -4,23 +4,27 @@ import Slider from "react-slick";
 const scrollList = [
   {
     image:
-      "https://Digiswarm.com/wp-content/uploads/2023/11/plant_mantra_logo-removebg-preview.png",
-  },
-  {
-    image: "https://Digiswarm.com/wp-content/uploads/2023/11/10.0.png",
-  },
-  {
-    image: "https://Digiswarm.com/wp-content/uploads/2023/11/9.0.png",
+      "https://res-console.cloudinary.com/dpzhezt6x/thumbnails/v1/image/upload/v1718372165/TG9nb18xX3A5cnkwbw==/drilldown",
   },
   {
     image:
-      "https://Digiswarm.com/wp-content/uploads/2023/09/Artboard-1-copy-s2.png",
+      "https://res.cloudinary.com/dpzhezt6x/image/upload/v1698570613/Untitled_design_23_qgnqja.png",
   },
   {
-    image: "https://Digiswarm.com/wp-content/uploads/2023/09/Layer-1.png",
+    image:
+      "https://res-console.cloudinary.com/dpzhezt6x/thumbnails/v1/image/upload/v1718372430/RHIuX0FydmluZF9DaGF1ZGhhcnlfMV9tczltZmk=/drilldown",
   },
   {
-    image: "https://Digiswarm.com/wp-content/uploads/2023/11/11.0.png",
+    image:
+      "https://res-console.cloudinary.com/dpzhezt6x/thumbnails/v1/image/upload/v1718373472/TmV3X0dhcmh3YWxfSmV3ZWxsZXJzX2gzYmpsNQ==/drilldown",
+  },
+  {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/scoot-production-a901c.appspot.com/o/exly%2Fe%2Fpng%3Bbas1XxBAueX?alt=media&token=62535d89-e460-474b-b045-d4b12200b923",
+  },
+  {
+    image:
+      "https://res-console.cloudinary.com/dpzhezt6x/thumbnails/v1/image/upload/v1718373796/SG90ZWxfMV9xZnN4c20=/drilldown",
   },
 ];
 
@@ -43,7 +47,7 @@ export default function AutoScroll() {
             return (
               <div key={indexs} className="img ">
                 <img
-                  className=" p-2 cursor-pointern px-6 "
+                  className=" p-2 w-64 cursor-pointern px-6 "
                   src={items.image}
                   alt=""
                 />
